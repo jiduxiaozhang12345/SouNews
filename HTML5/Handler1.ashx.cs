cc986cb988ec289e72aa3f11fc0bdbcdc553774b
jiduxiaozhang12345 <467677340@qq.com>
2015-12-14 18:02:58 +0800

    1.0 version

---
+++ HTML5/Handler1.ashx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace HTML5 {
+    /// <summary>
+    /// Handler1 的摘要说明
+    /// </summary>
+    public class Handler1 : IHttpHandler {
+
+        public void ProcessRequest(HttpContext context) {
+            context.Response.ContentType = "text/html";
+            int aa = Thread.CurrentThread.ManagedThreadId;
+            Thread.Sleep(5);
+            int bb = Thread.CurrentThread.ManagedThreadId;
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable {
+            get {
+                return false;
+            }
+        }
+    }
+}
---
+++ HTML5/JS/RelationMethod.js
@@ -0,0 +1,28 @@
+﻿function saveStorage() {
+    var data = new Object;
+    data.name = document.getElementById('name').value;
+    data.age = document.getElementById('age').value;
+    data.tel = document.getElementById('tel').value;
+    var str = JSON.stringify(data);
+    localStorage.setItem(data.name, str);
+    alert("保存成功");
+}
+
+function findStorage(id) {
+    var find = document.getElementById(id).value;
+    var result = localStorage.getItem(find);
+    if (result == null) {
+        alert("此数据不存在");
+        return false;
+    }
+    var str = JSON.parse(result);
+    var target = document.getElementById("msg");
+    target.innerHTML = "<br/>姓名：" + str.name + "<br/>年龄：" + str.age + "<br/>电话：" + str.tel;
+}
+
+function clearStorage(id) {
+    var find = document.getElementById(id).value;
+    localStorage.removeItem(find);
+    alert("删除成功");
+}
+
